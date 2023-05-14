23e912bdaa0ac70c278bf1a85726f4485ffe0dd1
Igor0390 <118315204+Igor0390@users.noreply.github.com>
2023-05-14 12:06:10 +0200

    Тз 9. Готово

---
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.filmorate.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+@RestController
+@Slf4j
+@RequestMapping("/User")
+public class UserController {
+    private HashMap<Integer, User> users = new HashMap<>();
+    @PostMapping
+    public User addUser(@Valid @RequestBody User user) {
+        log.info("add user");
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User updateUser(@Valid @RequestBody User user) {
+        log.info("update user");
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        log.info("get all users");
+        return new ArrayList<>(users.values());
+    }
+}
