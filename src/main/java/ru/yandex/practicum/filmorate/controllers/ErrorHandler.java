897510ffd04874201fe66b332602b18bf9467c43
Igor0390 <118315204+Igor0390@users.noreply.github.com>
2023-05-31 00:08:31 +0200

    редактирование class ErrorHandler

--- src/main/java/ru/yandex/practicum/filmorate/controllers/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exeption.NotFoundException;
@@ -11,21 +12,21 @@ import ru.yandex.practicum.filmorate.model.ErrorResponse;
 @RestControllerAdvice
 @Slf4j
 public class ErrorHandler {
-    @org.springframework.web.bind.annotation.ExceptionHandler
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFound(final NotFoundException e) {
         log.error("Not Found Exception");
         return new ErrorResponse(e.getMessage());
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidate(final ValidationException e) {
         log.error("Validation Exception");
         return new ErrorResponse(e.getMessage());
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(Throwable.class)
+    @ExceptionHandler(Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleException() {
         log.error("Неизвестная ошибка");
