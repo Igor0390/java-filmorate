de6694dba4abb581256902db485196191883ee60
Igor0390 <118315204+Igor0390@users.noreply.github.com>
2023-06-03 15:53:45 +0200

    все ValidationException от javax.validation.ValidationException

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -4,9 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import javax.validation.ValidationException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
